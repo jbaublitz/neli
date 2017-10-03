fc277811657e2bee20b4a5b719ef137993981dbc
John Baublitz <john.m.baublitz@gmail.com>
2017-10-03 18:50:16 -0400

    Add ability to set netlink ID and update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
-//! # nl - Type safety for the weary netlink user
+//! # Type safety for the weary netlink user
 //! 
 //! ## Rationale
 //! 
 //! The `libc` crate currently provides an interface for sockets but
-//! the constants to configure the socket to do anything useful in netlink,
-//! because they live in `/usr/include/linux/netlink.h` and friends,
-//! are not included in the crate. As a result, doing anything with netlink in Rust
+//! the constants to configure the socket to do anything useful in netlink
+//! are not included in the crate because they live in `/usr/include/linux/netlink.h` and friends.
+//! As a result, doing anything with netlink in Rust is currently a bit of a headache.
+//! 
+//! This crate aims to define the necessary constants and wrap them in types to both take
+//! advantage of the Rust type system and also avoid the need to pop open `.h` files
+//! to find the information necessary to construct netlink messages.
+//! 
+//! This is all further helped by `serde` which provides the underlying layer for byte
+//! serialization and deserialization used to convert messages from native Rust data
+//! structures avoiding `[#repr(C)]` to a byte slice compatible with the libc API.
 //! 
 //! ## Notes
 //! 
 //! This crate is currently under heavy development.
+//! 
+//! The `cc` crate is a build dependency to provide as much of a natively cross distribution
+//! approach as possible regarding `#define`s in C. It is used to compile a C file that includes
+//! the appropriate headers and exports them to the corresponding `stdint.h` types in C.
+//! This is then linked into Rust to expose them to the `ffi.rs` module. Big thanks to the
+//! `serde` people for working with me on my pull request to make this ever so much simpler
+//! to implement.
 
 extern crate libc;
 #[macro_use]
@@ -18,10 +33,17 @@ extern crate serde;
 extern crate serde_derive;
 extern crate byteorder;
 
+/// C constants defined as types
 pub mod ffi;
+/// Wrapper for `libc` sockets
 pub mod socket;
+/// Top-level netlink header
 pub mod nlhdr;
+/// Genetlink (generic netlink) header and attribute helpers
 pub mod genlhdr;
+/// `serde` serializaton implementation
 pub mod ser;
+/// `serde` deserializaton implementation
 pub mod de;
+/// Errors and conversions
 pub mod err;
--- src/socket.rs
+++ src/socket.rs
@@ -1,15 +1,31 @@
+//! # Socket code around `libc`
+//! 
+//! ## Notes
+//! 
+//! This module provides a low level one-to-one mapping between `libc` system call wrappers
+//! with defaults specific to netlink sockets as well as a higher level API for simplification
+//! of netlink code.
+
 use std::io;
 use std::mem::{zeroed,size_of};
 
 use libc::{self,c_int,c_void};
+use serde::{Serialize,Deserialize};
+use serde::de::DeserializeOwned;
 
 use ffi::NlFamily;
+use nlhdr::NlHdr;
+use ser::NlSerializer;
+use de::NlDeserializer;
+use err::NlError;
 
+/// Handle for the socket file descriptor
 pub struct NlSocket {
     fd: c_int,
 }
 
 impl NlSocket {
+    /// Wrapper around `socket()` syscall filling in the netlink-specific information
     pub fn new(proto: NlFamily) -> Result<Self, io::Error> {
         let proto_u32: u32 = proto.into();
         let fd = match unsafe {
@@ -21,11 +37,13 @@ impl NlSocket {
         Ok(NlSocket { fd: try!(fd) })
     }
 
-    pub fn bind(&mut self, groups: u32) -> Result<(), io::Error> {
+    /// Use this function to bind to a netlink ID and subscribe to groups. See netlink(7)
+    /// man pages for more information on netlink IDs and groups.
+    pub fn bind(&mut self, pid: Option<u32>, groups: Option<u32>) -> Result<(), io::Error> {
         let mut nladdr = unsafe { zeroed::<libc::sockaddr_nl>() };
         nladdr.nl_family = libc::AF_NETLINK as u16;
-        nladdr.nl_pid = 0;
-        nladdr.nl_groups = groups;
+        nladdr.nl_pid = pid.unwrap_or(0);
+        nladdr.nl_groups = groups.unwrap_or(0);
         match unsafe {
             libc::bind(self.fd, &nladdr as *const _ as *const libc::sockaddr,
                        size_of::<libc::sockaddr_nl>() as u32)
@@ -35,6 +53,8 @@ impl NlSocket {
         }
     }
 
+    /// Send message encoded as byte slice to the netlink ID specified in the netlink header
+    /// (`nl::nlhdr::NlHdr`).
     pub fn send(&mut self, buf: &[u8], flags: i32) -> Result<isize, io::Error> {
         match unsafe {
             libc::send(self.fd, buf as *const _ as *const c_void, buf.len(), flags)
@@ -44,8 +64,12 @@ impl NlSocket {
         }
     }
 
-    pub fn recv(&mut self, len: usize, flags: i32) -> Result<Vec<u8>, io::Error> {
-        let mut v = Vec::with_capacity(len); 
+    /// Receive message encoded as byte slice from the netlink socket.
+    pub fn recv(&mut self, len: Option<usize>, flags: i32) -> Result<Vec<u8>, io::Error> {
+        let mut v = match len {
+            Some(l) => Vec::with_capacity(l),
+            None => Vec::new(),
+        };
         match unsafe {
             libc::recv(self.fd, v.as_mut_slice() as *mut _ as *mut c_void, v.len(), flags)
         } {
@@ -54,15 +78,34 @@ impl NlSocket {
         }
     }
 
-    // Higher level API
-    pub fn connect(proto: NlFamily, groups: u32) -> Result<Self, io::Error> {
+    /// Equivalent of `socket` and `bind` calls.
+    pub fn connect(proto: NlFamily, pid: Option<u32>, groups: Option<u32>)
+                   -> Result<Self, io::Error> {
         let mut s = try!(NlSocket::new(proto));
-        try!(s.bind(groups));
+        try!(s.bind(pid, groups));
         Ok(s)
+    }
+
+    /// Serialize and send Rust `NlMsg` type
+    pub fn sendmsg<T: Serialize>(&mut self, msg: NlHdr<T>, flags: i32) -> Result<isize, NlError> {
+        let mut ser = NlSerializer::new();
+        try!(msg.serialize(&mut ser));
+        let len = try!(self.send(&ser.into_inner(), flags));
+        Ok(len)
+    }
+
+    /// Receive and deserialize Rust `NlMsg` type
+    pub fn recvmsg<T: DeserializeOwned>(&mut self, len: Option<usize>, flags: i32)
+                                       -> Result<NlHdr<T>, NlError> {
+        let buf = try!(self.recv(len, flags));
+        let mut de = NlDeserializer::new(&buf);
+        let msg = try!(NlHdr::<T>::deserialize(&mut de));
+        Ok(msg)
     }
 }
 
 impl Drop for NlSocket {
+    /// Closes underlying file descriptor to avoid file descriptor leaks.
     fn drop(&mut self) {
         unsafe { libc::close(self.fd); }
     }
@@ -74,7 +117,7 @@ mod test {
 
     #[test]
     fn test_socket_creation() {
-        match NlSocket::connect(NlFamily::NlGeneric, 0) {
+        match NlSocket::connect(NlFamily::NlGeneric, None, None) {
             Err(_) => panic!(),
             _ => (),
         }
