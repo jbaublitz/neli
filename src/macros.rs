e299558febe770827b91b2f194033f77c8a751c8
John Baublitz <john.m.baublitz@gmail.com>
2018-04-24 09:28:53 -0400

    Restructure macros for cmd specific macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,23 +1,24 @@
 #[macro_export]
-macro_rules! attr_cmd_to_nl {
-    ( $name:ident, $( $cmd:ident ),* ) => {
+
+macro_rules! enum_to_nl {
+    ( $name:ident, $type:tt, $( $cmd:ident ),* ) => {
         #[derive(Clone,Debug,PartialEq)]
         pub enum $name {
             $( $cmd, )*
             UnrecognizedVariant,
         }
 
-        impl Into<u8> for $name {
-            fn into(self) -> u8 {
-                self as u8
+        impl Into<$type> for $name {
+            fn into(self) -> $type {
+                self as $type
             }
         }
 
-        impl From<u8> for $name {
-            fn from(v: u8) -> Self {
+        impl From<$type> for $name {
+            fn from(v: $type) -> Self {
                 match v {
                     $(
-                        i if i == $name::$cmd as u8 => $name::$cmd,
+                        i if i == $name::$cmd as $type => $name::$cmd,
                     )*
                     _ => $name::UnrecognizedVariant,
                 }
@@ -29,20 +30,27 @@ macro_rules! attr_cmd_to_nl {
             type DeIn = ();
 
             fn serialize(&self, mem: &mut MemWrite) -> Result<(), SerError> {
-                let val = self.clone() as u8;
+                let val = self.clone() as $type;
                 val.serialize(mem)?;
                 Ok(())
             }
 
             fn deserialize(mem: &mut MemRead) -> Result<Self, DeError> {
-                let cmd = u8::deserialize(mem)?;
+                let cmd = $type::deserialize(mem)?;
                 Ok(Self::from(cmd))
             }
 
             fn size(&self) -> usize {
-                0u8.size()
+                ::std::mem::size_of::<$type>()
             }
         }
+    }
+}
+
+#[macro_export]
+macro_rules! cmd_to_nl {
+    ( $name:ident, $( $cmd:ident ),* ) => {
+        enum_to_nl!($name, u8, $( $cmd ),*);
     }
 }
 
@@ -53,7 +61,7 @@ mod test {
 
     #[test]
     fn test_attr_cmd_to_nl_macro() {
-        attr_cmd_to_nl!(TestCmd, Cmd0, Cmd1, Cmd2, Cmd3);
+        cmd_to_nl!(TestCmd, Cmd0, Cmd1, Cmd2, Cmd3);
 
         let slice = &mut [0u8; 1];
         let mut ser_mem = MemWrite::new_slice(slice);
