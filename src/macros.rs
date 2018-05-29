1925ba1b3d876f981d0817bbf28ec22b311e6dcf
John Baublitz <john.m.baublitz@gmail.com>
2018-05-28 23:27:33 -0400

    Rework macros to allow choosing relevant enum options

--- src/macros.rs
+++ src/macros.rs
@@ -44,14 +44,61 @@ macro_rules! enum_to_nl {
                 ::std::mem::size_of::<$type>()
             }
         }
-    }
+    };
+    ( $name:ident, $type:tt, $( $cmd:ident = $num:tt ),* ) => {
+        #[derive(Clone,Debug,PartialEq)]
+        pub enum $name {
+            $( $cmd = $num, )*
+            UnrecognizedVariant,
+        }
+
+        impl Into<$type> for $name {
+            fn into(self) -> $type {
+                self as $type
+            }
+        }
+
+        impl From<$type> for $name {
+            fn from(v: $type) -> Self {
+                match v {
+                    $(
+                        i if i == $name::$cmd as $type => $name::$cmd,
+                    )*
+                    _ => $name::UnrecognizedVariant,
+                }
+            }
+        }
+
+        impl Nl for $name {
+            type SerIn = ();
+            type DeIn = ();
+
+            fn serialize(&self, mem: &mut MemWrite) -> Result<(), SerError> {
+                let val = self.clone() as $type;
+                val.serialize(mem)?;
+                Ok(())
+            }
+
+            fn deserialize(mem: &mut MemRead) -> Result<Self, DeError> {
+                let cmd = $type::deserialize(mem)?;
+                Ok(Self::from(cmd))
+            }
+
+            fn size(&self) -> usize {
+                ::std::mem::size_of::<$type>()
+            }
+        }
+    };
 }
 
 #[macro_export]
 macro_rules! cmd_to_nl {
     ( $name:ident, $( $cmd:ident ),* ) => {
         enum_to_nl!($name, u8, $( $cmd ),*);
-    }
+    };
+    ( $name:ident, $( $cmd:ident = $num:tt ),* ) => {
+        enum_to_nl!($name, u8, $( $cmd = $num ),*);
+    };
 }
 
 #[cfg(test)]
