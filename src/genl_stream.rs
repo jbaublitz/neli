f212737e87302626a3d5dd90cf03dcdc63874b5d
John Baublitz <john.m.baublitz@gmail.com>
2019-05-09 00:46:03 -0400

    Allow example genl_stream to take arguments to specify multicast group

--- src/genl_stream.rs
+++ src/genl_stream.rs
@@ -4,6 +4,8 @@ extern crate neli;
 #[cfg(feature = "stream")]
 extern crate tokio;
 
+use std::env;
+
 use neli::consts;
 use neli::genl::Genlmsghdr;
 use neli::socket;
@@ -11,10 +13,21 @@ use neli::socket;
 use tokio::prelude::{Future,Stream};
 
 #[cfg(feature = "stream")]
-fn debug_acpi_stream() -> Result<(), neli::err::NlError> {
+fn debug_stream() -> Result<(), neli::err::NlError> {
+    let mut args = env::args();
+    let _ = args.next();
+    let first_arg = args.next();
+    let second_arg = args.next();
+    let (family_name, mc_group_name) = match (first_arg, second_arg) {
+        (Some(fam_name), Some(mc_name)) => (fam_name, mc_name),
+        (_, _) => {
+            println!("USAGE: genl_stream FAMILY_NAME MULTICAST_GROUP_NAME");
+            std::process::exit(1)
+        },
+    };
     let mut s = socket::NlSocket::connect(consts::NlFamily::Generic,
                                           None, None, true)?;
-    let id = s.resolve_nl_mcast_group("acpi_event", "acpi_mc_group")?;
+    let id = s.resolve_nl_mcast_group(&family_name, &mc_group_name)?;
     s.set_mcast_groups(vec![id])?;
     let ss = neli::socket::tokio::NlSocket::<u16, Genlmsghdr<u8>>::new(s)?;
     tokio::run(ss.for_each(|next| {
@@ -25,10 +38,21 @@ fn debug_acpi_stream() -> Result<(), neli::err::NlError> {
 }
 
 #[cfg(not(feature = "stream"))]
-fn debug_acpi_stream() -> Result<(), neli::err::NlError> {
+fn debug_stream() -> Result<(), neli::err::NlError> {
+    let mut args = env::args();
+    let _ = args.next();
+    let first_arg = args.next();
+    let second_arg = args.next();
+    let (family_name, mc_group_name) = match (first_arg, second_arg) {
+        (Some(fam_name), Some(mc_name)) => (fam_name, mc_name),
+        (_, _) => {
+            println!("USAGE: genl_stream FAMILY_NAME MULTICAST_GROUP_NAME");
+            std::process::exit(1)
+        },
+    };
     let mut s = socket::NlSocket::connect(consts::NlFamily::Generic,
                                           None, None, true)?;
-    let id = s.resolve_nl_mcast_group("acpi_event", "acpi_mc_group")?;
+    let id = s.resolve_nl_mcast_group(&family_name, &mc_group_name)?;
     s.set_mcast_groups(vec![id])?;
     for next in s.iter::<u16, Genlmsghdr<u8>>() {
         println!("{:?}", next);
@@ -38,14 +62,14 @@ fn debug_acpi_stream() -> Result<(), neli::err::NlError> {
 
 pub fn main() {
     #[cfg(feature = "stream")]
-    match debug_acpi_stream() {
+    match debug_stream() {
         Ok(_) => (),
         Err(e) => {
             println!("{}", e);
         }
     };
     #[cfg(not(feature = "stream"))]
-    match debug_acpi_stream() {
+    match debug_stream() {
         Ok(_) => (),
         Err(e) => {
             println!("{}", e);
