231f0376e26e25c195345afff9647aaf64c07b0e
John Baublitz <john.m.baublitz@gmail.com>
2019-06-03 15:36:27 -0400

    Additional example of creating nested attributes

---
+++ examples/create-nested-attributes.rs
@@ -0,0 +1,21 @@
+extern crate neli;
+
+use std::error::Error;
+
+use neli::Nl;
+use neli::consts;
+use neli::nl::Nlmsghdr;
+use neli::genl::Genlmsghdr;
+use neli::nlattr::Nlattr;
+
+pub fn main() -> Result<(), Box<Error>> {
+    let attrs = vec![Nlattr::new(None, 1, vec![Nlattr::new(None, 1, "this_family")]),
+                     Nlattr::new(None, 2, vec![Nlattr::new(None, 1, "that_family")])];
+    let genlmsg = Genlmsghdr::new(consts::CtrlCmd::Getfamily, 2, attrs)?;
+    let nlmsg = Nlmsghdr::new(None, consts::Nlmsg::Noop, vec![consts::NlmF::Request], None, None,
+                              genlmsg);
+    let mut buffer = neli::StreamWriteBuffer::new_growable(Some(nlmsg.asize()));
+    nlmsg.serialize(&mut buffer)?;
+    println!("{:?}", buffer.as_ref());
+    Ok(())
+}
