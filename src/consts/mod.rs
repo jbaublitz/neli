a3154379a341b093463133e98038ac2998a6f6cf
Alex Gavin <a_gavin@icloud.com>
2025-01-18 11:45:05 -0800

    WIP nl80211 code

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -57,6 +57,8 @@ pub mod mac80211_hwsim;
 pub mod netfilter;
 /// Constants related to generic netlink top level headers
 pub mod nl;
+/// Constants related to nl80211, Linux userspace API to wireless stack
+pub mod nl80211;
 /// Constants related to rtnetlink
 pub mod rtnl;
 /// Constants related to netlink socket operations
