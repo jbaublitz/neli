b81f6561cae52fc6335f290299743cdd1659664c
Bben01 <52465698+Bben01@users.noreply.github.com>
2025-06-03 19:25:07 +0300

    Use the new libc version

--- src/consts/connector.rs
+++ src/consts/connector.rs
@@ -4,12 +4,29 @@ use crate as neli;
 #[neli::neli_enum(serialized_type = "u32")]
 pub enum CnMsgIdx {
     Proc = libc::CN_IDX_PROC,
+    Cifs = libc::CN_IDX_CIFS,
+    W1 = libc::CN_W1_IDX,
+    V86d = libc::CN_IDX_V86D,
+    Bb = libc::CN_IDX_BB,
+    Dst = libc::CN_DST_IDX,
+    Dm = libc::CN_IDX_DM,
+    Drbd = libc::CN_IDX_DRBD,
+    Kvp = libc::CN_KVP_IDX,
+    Vss = libc::CN_VSS_IDX,
 }
 
 /// Values for `val` in [`CnMsg`][crate::connector::CnMsg].
 #[neli::neli_enum(serialized_type = "u32")]
 pub enum CnMsgVal {
     Proc = libc::CN_VAL_PROC,
+    Cifs = libc::CN_VAL_CIFS,
+    W1 = libc::CN_W1_VAL,
+    V86dUvesafb = libc::CN_VAL_V86D_UVESAFB,
+    Dst = libc::CN_DST_VAL,
+    DmUserspaceLog = libc::CN_VAL_DM_USERSPACE_LOG,
+    Drbd = libc::CN_VAL_DRBD,
+    Kvp = libc::CN_KVP_VAL,
+    Vss = libc::CN_VSS_VAL,
 }
 
 /// Process event type as reported by the kernel connector.
